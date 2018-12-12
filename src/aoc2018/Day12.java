f0fdd25b4cfa1f539638ebc2a3604632b5fcd175
Marcel Steffen <marcel@talentsconnect.com>
2018-12-12 21:28:37 +0100

    add solution for 2018 Day 11 part 2 - a little less DIRTY

--- src/aoc2018/Day12.java
+++ src/aoc2018/Day12.java
@@ -3,22 +3,21 @@ package aoc2018;
 import aoc.Day;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * Optimized version using https://en.wikipedia.org/wiki/Summed-area_table
- *
- * Inspired by https://www.reddit.com/r/adventofcode/comments/a53r6i/2018_day_11_solutions/
- */
 public class Day12 extends Day {
 
     public Day12(String year, String day) {
         super(year, day);
     }
+
+    private String initialState;
+    private String state;
+    private int negativePots;
+    private Map<String, String> mutations;
 
     @Override
     protected void part1(List<String> inputs) {
@@ -27,14 +26,12 @@ public class Day12 extends Day {
         Matcher matcher = initialPattern.matcher(inputs.get(0));
 
         matcher.find();
-        String initialState = matcher.group(1);
-        initialState = "...." + initialState + "....";
-        int negativPots = 4;
+        state = matcher.group(1);
 
         Pattern mutationPattern = Pattern.compile("(.....) => (.)");
         Matcher mutationMatcher;
 
-        Map<String, String> mutations = new HashMap<>();
+        mutations = new HashMap<>();
 
         for (String input : inputs.subList(2, inputs.size())) {
             mutationMatcher = mutationPattern.matcher(input);
@@ -43,62 +40,85 @@ public class Day12 extends Day {
             mutations.put(mutationMatcher.group(1), mutationMatcher.group(2));
         }
 
-        System.out.println(initialState);
-        for (int i = 0; i < 20; i++) {
-            String copy = initialState.replaceAll("#", ".");
+        initialState = state;
+
+        printSolution(1, String.valueOf(runGenerations(20)));
+
+    }
+
+    @Override
+    protected void part2(List<String> inputs) {
+        // at sometime the generations mutate on a stable pattern. For me this was at 112
+        double stable = runGenerations(112);
+        // so we only shift to the right an the increase each generation is just as high as the plant count
+        double solution = stable + ((50000000000d - 112) * plantCount);
+
+        printSolution(2, String.valueOf(solution));
+    }
+
+    private double runGenerations(double amount) {
+        state = "...." + initialState + "....";
+        negativePots = 4;
+        for (double i = 0; i < amount; i++) {
+
+            String copy = state.replaceAll("#", ".");
 
             for (Map.Entry<String, String> entry : mutations.entrySet()) {
                 if (entry.getValue().equals(".")) {
                     continue;
                 }
-                int index = initialState.indexOf(entry.getKey());
-                while (index != -1 ) {
+                int index = state.indexOf(entry.getKey());
+                while (index != -1) {
                     copy = copy.substring(0, index + 2) + entry.getValue() + copy.substring(index + 3);
-                    index = initialState.indexOf(entry.getKey(), index + 1);
+                    index = state.indexOf(entry.getKey(), index + 1);
                 }
             }
 
-            initialState = copy;
-
-            if (initialState.substring(0, 1).equals("#")) {
-                initialState = "...." + initialState;
-                negativPots += 4;
-            } else if (initialState.substring(1, 2).equals("#")){
-                initialState = "..." + initialState;
-                negativPots += 3;
-            } else if (initialState.substring(2, 3).equals("#")){
-                initialState = ".." + initialState;
-                negativPots += 2;
-            } else if (initialState.substring(3, 4).equals("#")){
-                initialState = "." + initialState;
-                negativPots += 1;
-            }
-
-            if (initialState.substring(initialState.length() - 1).equals("#")) {
-                initialState = initialState + "...";
-            } else if (initialState.substring(initialState.length() - 2, initialState.length() - 1).equals("#")) {
-                initialState = initialState + "..";
-            } else if (initialState.substring(initialState.length() - 3, initialState.length() - 2).equals("#")) {
-                initialState = initialState + ".";
-            }
-
-            System.out.println(initialState);
+            state = copy;
+            extendPotRange();
         }
 
-        int sum = 0;
-        char[] chars = initialState.toCharArray();
+        return sum();
+    }
+
+    int plantCount;
+
+    private double sum() {
+        plantCount = 0;
+        double sum = 0;
+        char[] chars = state.toCharArray();
         for (int i = 0; i < chars.length; i++) {
             char aChar = chars[i];
             if (aChar == '#') {
-                sum = sum + i - negativPots;
+                plantCount++;
+                sum = sum + i - negativePots;
             }
         }
 
-        printSolution(1, sum);
+        return sum;
     }
 
-    @Override
-    protected void part2(List<String> inputs) {
-    }
+    private void extendPotRange() {
+        if (state.substring(0, 1).equals("#")) {
+            state = "...." + state;
+            negativePots += 4;
+        } else if (state.substring(1, 2).equals("#")) {
+            state = "..." + state;
+            negativePots += 3;
+        } else if (state.substring(2, 3).equals("#")) {
+            state = ".." + state;
+            negativePots += 2;
+        } else if (state.substring(3, 4).equals("#")) {
+            state = "." + state;
+            negativePots += 1;
+        }
 
+        if (state.substring(state.length() - 1).equals("#")) {
+            state = state + "...";
+        } else if (state.substring(state.length() - 2, state.length() - 1).equals("#")) {
+            state = state + "..";
+        } else if (state.substring(state.length() - 3, state.length() - 2).equals("#")) {
+            state = state + ".";
+        }
+    }
 }
