fdf852ce563cf4557b393205a0a7a57c199be6b0
Marcel Steffen <marcel@talentsconnect.com>
2018-12-13 20:41:18 +0100

    add solution for 2018 Day 13

---
+++ src/aoc2018/Day13.java
@@ -0,0 +1,132 @@
+package aoc2018;
+
+import aoc.Day;
+import aoc2018.helper.Car;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day13 extends Day {
+
+    private char[][] tracks;
+    List<Car> cars;
+    private int carIndex = 0;
+
+    public Day13(String year, String day) {
+        super(year, day);
+    }
+
+    @Override
+    protected void part1(List<String> inputs) {
+        buildTrack(inputs);
+
+        printTrack();
+
+        Point crashSite;
+
+        trackOnline:
+        while (true) {
+            Collections.sort(cars, Car.getPositionComparator());
+
+            for (Car car : cars) {
+                Point position = car.move();
+                if (checkCrash(car)) {
+                    crashSite = position;
+                    break trackOnline;
+                }
+                car.checkTurn(tracks[position.x][position.y]);
+            }
+        }
+
+        printSolution(1, crashSite.x + "," + crashSite.y);
+    }
+
+    @Override
+    protected void part2(List<String> inputs) {
+        buildTrack(inputs);
+
+        while (true) {
+            cars = cars.stream().filter(c -> !c.isBroken()).collect(Collectors.toList());
+            Collections.sort(cars, Car.getPositionComparator());
+
+            if (cars.size() == 1) {
+                //cars.get(0).move();
+                break;
+            }
+
+            for (Car car : cars) {
+                if (car.isBroken()) {
+                    continue;
+                }
+
+                Point position = car.move();
+                if (checkCrash(car)) {
+                    continue;
+                }
+                car.checkTurn(tracks[position.x][position.y]);
+            }
+        }
+
+        Car remainingCar = cars.get(0);
+        printSolution(2, remainingCar.getX() + "," + remainingCar.getY());
+    }
+
+    private void printTrack() {
+        for (int y = 0; y < tracks[0].length; y++) {
+            String row = "";
+            for (int x = 0; x < tracks.length; x++) {
+                row += tracks[x][y];
+            }
+            System.out.println(row);
+        }
+    }
+
+    private boolean checkCrash(Car car) {
+        List<Car> otherCars = cars
+                .stream()
+                .filter(c -> c.getIndex() != car.getIndex())
+                .collect(Collectors.toList());
+
+        for (Car otherCar : otherCars) {
+            if (otherCar.getX() == car.getX() && otherCar.getY() == car.getY()) {
+                car.setBroken(true);
+                otherCar.setBroken(true);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    private void buildTrack(List<String> inputs) {
+        cars = new ArrayList<>();
+        int maxWidth = inputs.stream().mapToInt(s -> s.length()).max().orElse(0);
+        tracks = new char[maxWidth][inputs.size()];
+
+        int y = 0;
+        for (String input : inputs) {
+            for (int x = 0; x < maxWidth; x++) {
+                char trackTile;
+                try {
+                    trackTile = input.charAt(x);
+                } catch (StringIndexOutOfBoundsException e) {
+                    trackTile = ' ';
+                }
+                if (Car.isCar(trackTile)) {
+                    Car car = new Car(carIndex++, x, y, trackTile);
+                    tracks[x][y] = car.getInitialTrackTile();
+                    cars.add(car);
+                    continue;
+                }
+
+                tracks[x][y] = trackTile;
+            }
+
+            y++;
+        }
+    }
+}
---
+++ src/aoc2018/helper/Car.java
@@ -0,0 +1,166 @@
+package aoc2018.helper;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Car {
+    public static final List<Character> CAR_SYMBOLS = Arrays.asList('>', '<', 'v', '^');
+    private int index;
+    private int x;
+    private int y;
+    private char direction;
+    private LinkedList<Character> directions = new LinkedList<>();
+    private LinkedList<Character> turns = new LinkedList<>();
+    private boolean broken = false;
+
+    public Car(int index, int x, int y, char direction) {
+        this.index = index;
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+        directions.add('l');
+        directions.add('s');
+        directions.add('r');
+        turns.add('^');
+        turns.add('>');
+        turns.add('v');
+        turns.add('<');
+
+        while (direction != turns.getFirst()) {
+            turns.addLast(turns.removeFirst());
+        }
+    }
+
+    public static boolean isCar(char element) {
+        return CAR_SYMBOLS.contains(element);
+    }
+
+    public Point move() {
+        if (direction == '^') {
+            y--;
+        } else if (direction == '>') {
+            x++;
+        } else if (direction == 'v') {
+            y++;
+        } else if (direction == '<') {
+            x--;
+        }
+
+        return new Point(x, y);
+    }
+
+    public char getInitialTrackTile() {
+        if (direction == '>' || direction == '<') {
+            return '-';
+        } else {
+            return '|';
+        }
+    }
+
+    public void checkTurn(char trackTile) {
+        if (trackTile == '/' || trackTile == '\\') {
+            turnOnCurve(trackTile);
+        } else if (trackTile == '+') {
+            turnOnCrossing();
+        }
+    }
+
+    private void turnOnCurve(char trackTile) {
+        if (trackTile == '/') {
+            if (direction == '>' || direction == '<') {
+                turnLeft();
+            } else if (direction == '^' || direction == 'v') {
+                turnRight();
+            }
+        } else if (trackTile == '\\') {
+            if (direction == '>' || direction == '<') {
+                turnRight();
+            } else if (direction == '^' || direction == 'v') {
+                turnLeft();
+            }
+        }
+    }
+
+    private void turnOnCrossing() {
+        Character character = directions.removeFirst();
+
+        if (character == 'l') {
+            turnLeft();
+        } else if (character == 'r') {
+            turnRight();
+        }
+
+        directions.addLast(character);
+    }
+
+    private void turnLeft() {
+        turns.addFirst(turns.removeLast());
+
+        direction = turns.getFirst();
+    }
+
+    private void turnRight() {
+        turns.addLast(turns.removeFirst());
+
+        direction = turns.getFirst();
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public char getDirection() {
+        return direction;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setDirection(char direction) {
+        this.direction = direction;
+    }
+
+    public static Comparator<Car> getPositionComparator() {
+        return (car1, car2) -> {
+            Integer y1 = car1.getY();
+            Integer y2 = car2.getY();
+            int yComp = y1.compareTo(y2);
+
+            if (yComp != 0) {
+                return yComp;
+            }
+
+            Integer x1 = car1.getX();
+            Integer x2 = car2.getY();
+            return x1.compareTo(x2);
+        };
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public boolean isBroken() {
+        return broken;
+    }
+
+    public void setBroken(boolean broken) {
+        this.broken = broken;
+    }
+}
